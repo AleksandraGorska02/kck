56afe3fd8153670ba16db1bd01419100ffb88ab2
Aleksandra Gorska <aleksandra.gorska02@gmail.com>
2023-12-12 09:41:02 +0100

    70%

--- zadanie_3_2/rozpoznawanie.py
+++ zadanie_3_2/rozpoznawanie.py
@@ -2,50 +2,67 @@ import scipy
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-
+from pydub import AudioSegment
 from numpy import *
 import math
 from ipywidgets import *
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def hps(signal,fs):
-   
-    signal1 = signal[0:fs]
-    #fft na signal1
-    for i in range(0,fs):
-        signal1[i] = fft.fft(signal1[i])
-    #fft na signal2
-    plt.plot(signal1)
-    plt.show()
-    
-
-   
-
-    return signal
-  
 
 
 
+def hps(signal, fs):
+    """Estimate the pitch using the Harmonic Product Spectrum (HPS).
+    """
+    # Compute the short-time Fourier transform of the signal
+
+
+    hamming_window = np.hamming(len(signal))
+    signal = signal * hamming_window
+    spectrum = np.fft.rfft(signal)
+    spectrum = np.abs(spectrum)
+    hps = spectrum.copy()
+    for harmonic in range(2, 6): #wywalenie wszystkicg równych 0
+        decimated_spectrum = spectrum[::harmonic]
+        hps[:len(decimated_spectrum)] *= decimated_spectrum
+    # Find the peak in the HPS
 
 
 
+    estimated_pitch_index = np.argmax(hps)
+
+    # Convert the index to frequency in Hertz
+
+    estimated_pitch = fs * estimated_pitch_index / len(signal)
+
+    return estimated_pitch
+        
 if __name__ == '__main__':
- 
+    wyn=0
     files_path = os.listdir('./zadanie_3_2/train/')
-    print(files_path)
     for file in files_path:
        
         fs, signal = scipy.io.wavfile.read("./zadanie_3_2/train/"+file, mmap=False)
+        if len(signal.shape) > 1:
+            signal = signal[:, 0]
+
         #correct answer
         correrct_answer = file.split('_')[1][0]
-        hpss = hps(signal,fs)
-        if hpss == correrct_answer:
-            print('Correct')
-            print(hpss)
+      
+       
+        result ="K" if hps(signal, fs) > 160 else "M"
+        if result == correrct_answer:
+            wyn+=1
         else:
-            print('Incorrect')
-            print(hpss)
+            print("FAŁsz")
+     
+
+
+       
+        print("result: ", result, "correct answer: ", correrct_answer, "file: ", file, "hps: ", hps(signal, fs))
+        
+    print(wyn / len(files_path))
 
         
  
